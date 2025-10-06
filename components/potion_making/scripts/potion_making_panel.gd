98b9d99b48b19a9daf0f44d3974d92da76b3ed5f
Victor Guerra <wasmon999@hotmail.com>
2025-10-06 16:43:17 -0600

    ASCT-35 Initial implementation of the panel

--- components/potion_making/scripts/potion_making_panel.gd
+++ components/potion_making/scripts/potion_making_panel.gd
@@ -1,13 +1,14 @@
 extends Control
 class_name UIPotionMakingPanel
 
-@export var state_machine: StateMachine
+@export var selection_state_machine: StateMachine
+@export var ingredient_state_machine: StateMachine
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	state_machine.init(self)
-
+	selection_state_machine.init(self)
+	ingredient_state_machine.init(self)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- components/potion_making/states/cook_button.gd
+++ components/potion_making/states/cook_button.gd
@@ -4,13 +4,14 @@ class_name PotionMakingCookButtonState
 ## The UI node to which this state is associated with,
 ## so that it can control it directly
 @export var ui_element: Control
-
 @export var connected_states: Array[State]
 
 @onready var states_dict: Dictionary = {
 	"enter" = connected_states[0],
 	"down" = connected_states[1]
 }
+
+signal cook_potion
 
 func enter() -> void:
 	ui_element.enable_hand()
@@ -22,7 +23,7 @@ func process_input(event: InputEvent) -> State:
 	if event.is_action_pressed("menu_down"):
 		return states_dict["down"]
 	elif event.is_action_pressed("menu_enter"):
-		return states_dict["enter"]
+		emit_signal("cook_potion")
 		
 	return null
 
